aab88724714680eef76d684d9c5b88a35ff07faa
Rodrigo Rios <rodrigo.rras@gmail.com>
2020-05-14 14:16:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi There, message by Rios ! 2020,  !
+          What would it take Universe. for I get this amazing job and salary now? 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
